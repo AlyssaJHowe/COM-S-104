b8a808691f6b4e010658549bb46463e886501ac9
Alyssa Howe <35346343+AlyssaJHowe@users.noreply.github.com>
2018-01-11 13:47:59 -0600

    Delete dog_years(4).py

--- Lab 2/dog_years(4).py
+++
@@ -1,6 +0,0 @@
-# Simple interactive script figures out a person's age # in dog years
-name = raw_input("What's your name? ")
-print "Hi", name
-age = input("How old are you? ")
-dog_years = age / 7
-print "Well,", name, ", in dog years you are only", dog_years, "!"
